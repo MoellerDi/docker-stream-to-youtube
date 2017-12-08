f40a6e54f6093c6978902d5ec43e09e9aa1f1a24
Dirk Moeller <dirkmoeller@Dirks-MBP.lan>
2017-12-08 11:03:42 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,4 @@ WORKDIR /root/
 COPY entry.sh	.
 RUN chmod +x entry.sh
 
-## --- ENV ---
-ENV \
-    INPUT_VIDEO=http://octopi.local:8080/?action=stream \
-    INPUT_VCODEC=h264 \
-    INPUT_AUDIO=/dev/zero \
-    INPUT_ACODEC=aac
-
 ENTRYPOINT ["./entry.sh"]
--- entry.sh
+++ entry.sh
@@ -2,40 +2,30 @@
 
 echo Live-stream secret: $1
 
-echo Live-stream INPUT_VIDEO: ${INPUT_VIDEO}
-echo Live-stream INPUT_VCODEC: ${INPUT_VCODEC}
-echo Live-stream INPUT_AUDIO: ${INPUT_AUDIO}
-echo Live-stream INPUT_ACODEC: ${INPUT_ACODEC}
+echo Live-stream INPUT_AUDIO_FORMAT: ${INPUT_AUDIO_FORMAT}
+echo Live-stream INPUT_AUDIO_STREAM: ${INPUT_AUDIO_STREAM}
 
-# ffmpeg -f mjpeg \
-#   -i ${INPUT_VIDEO} \
-#   -an \
-#   -vcodec ${INPUT_VCODEC} \
-#   -ar 44100 \
-#   -ac 2 \
-#   -acodec pcm_s16le \
-#   -f s16le \
-#   -ac 2 \
-#   -i ${INPUT_AUDIO} \
-#   -acodec ${INPUT_ACODEC} \
-#   -ab 128k \
-#   -g 50 \
-#   -strict experimental \
-#   -f flv rtmp://a.rtmp.youtube.com/live2/$1
+echo Live-stream INPUT_VIDEO_FORMAT: ${INPUT_VIDEO_FORMAT}
+echo Live-stream INPUT_VIDEO_STREAM: ${INPUT_VIDEO_STREAM}
+
+echo Live-stream OUTPUT_ACODEC: ${OUTPUT_ACODEC}
+echo Live-stream OUTPUT_VCODEC: ${OUTPUT_VCODEC}
 
 avconv \
-  -re \
-  -ar 44100 \
-  -ac 2 \
+  -re \ # read input at native frame rate
+  -ar 44100 \ # set audio sampling rate (in Hz)
+  -ac 2 \ # set number of audio channels
+
+  -f ${INPUT_AUDIO_FORMAT} \ # Force input format
+  -i ${INPUT_AUDIO_STREAM} \ # input source
   -acodec pcm_s16le \
-  -f s16le \
-  -ac 2 \
-  -i ${INPUT_AUDIO} \
-  -f mjpeg \
-  -i ${INPUT_VIDEO} \
-  -vcodec ${INPUT_VCODEC} \
-  -acodec ${INPUT_ACODEC} \
+
+  -f ${INPUT_VIDEO_FORMAT} \ # Force input format 
+  -i ${INPUT_VIDEO_STREAM} \ # input source
+
+  -vcodec ${OUTPUT_VCODEC} \ # Set the video codec
+  -acodec ${OUTPUT_ACODEC} \ # Set the audio codec
   -ab 128k \
-  -g 50 \
-  -strict experimental \
+  -g 50 \ # set the group of picture (GOP) size
+  -strict experimental \ # allow non-standardized experimental things
   -f flv rtmp://a.rtmp.youtube.com/live2/$1
