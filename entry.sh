14588253dfc2192ab45e2ac03aa646bf046c5d0c
Dirk Moeller <dirkmoeller@Dirks-MBP.lan>
2017-12-08 11:12:39 +0100

    update

--- entry.sh
+++ entry.sh
@@ -12,20 +12,20 @@ echo Live-stream OUTPUT_ACODEC: ${OUTPUT_ACODEC}
 echo Live-stream OUTPUT_VCODEC: ${OUTPUT_VCODEC}
 
 avconv \
-  -re \ # read input at native frame rate
-  -ar 44100 \ # set audio sampling rate (in Hz)
-  -ac 2 \ # set number of audio channels
+  -re \
+  -ar 44100 \
+  -ac 2 \
 
-  -f ${INPUT_AUDIO_FORMAT} \ # Force input format
-  -i ${INPUT_AUDIO_STREAM} \ # input source
+  -f ${INPUT_AUDIO_FORMAT} \
+  -i ${INPUT_AUDIO_STREAM} \
   -acodec pcm_s16le \
 
-  -f ${INPUT_VIDEO_FORMAT} \ # Force input format 
-  -i ${INPUT_VIDEO_STREAM} \ # input source
+  -f ${INPUT_VIDEO_FORMAT} \
+  -i ${INPUT_VIDEO_STREAM} \
 
-  -vcodec ${OUTPUT_VCODEC} \ # Set the video codec
-  -acodec ${OUTPUT_ACODEC} \ # Set the audio codec
+  -vcodec ${OUTPUT_VCODEC} \
+  -acodec ${OUTPUT_ACODEC} \
   -ab 128k \
-  -g 50 \ # set the group of picture (GOP) size
-  -strict experimental \ # allow non-standardized experimental things
+  -g 50 \
+  -strict experimental \
   -f flv rtmp://a.rtmp.youtube.com/live2/$1
